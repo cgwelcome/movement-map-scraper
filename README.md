5d5bfcf2bce4ce81a9762a6687ec8f805b7dd40a
Charles Gao <charlesgao.c@gmail.com>
2017-10-22 13:19:47 -0400

    Readme update

---
+++ README.md
@@ -0,0 +1,5 @@
+## Getting Stated
+On Python 2.7,
+1. Install a virtual sandbox
+2. `pip install -r requirements.txt`
+3. `python scraper.py [Google Place API] [Database password] [Starting_entry] [Size]`
--- scraper.py
+++ scraper.py
@@ -1,10 +1,19 @@
 from bs4 import BeautifulSoup
 from googleplaces import GooglePlaces
 import MySQLdb
+import json
 import re
 import requests
 import sys
-from pprint import pprint
+
+def getinstagram(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    twitter_link = soup.find('a', href=re.compile('www.instagram'))
+    if twitter_link:
+        return twitter_link.get('href')
+    else:
+        return None
 
 
 def gettwitter(url):
@@ -35,30 +44,58 @@ def getinfo(church_name, location, postalcode):
         return place
 
 
-def handler_event(event, context):
-    db = MySQLdb.connect(user="MovementAdmin", passwd="CityMovement!", host="movementmap.cjtdrwjqblil.us-east-1.rds.amazonaws.com", db= "MovementMap")
+def handler_event():
+    db = MySQLdb.connect(user="MovementAdmin", passwd=sys.argv[2], host="movementmap.cjtdrwjqblil.us-east-1.rds.amazonaws.com", db= "MovementMap")
     cursor = db.cursor()
-    cursor.execute('SELECT Legal_Name, S, Postal_Code FROM MovementMapExcelImport WHERE Legal_Name LIKE (\'%church%\') ORDER BY Rev_2014 DESC')
 
     info_json = []
-    for x in range(0, 5):
+    for y in range(0, int(sys.argv[4])):
         url = ""
+
+        offset = str(y + int(sys.argv[3]))
+        lst = []
         try:
+            cursor.execute('SELECT Legal_Name, S, Postal_Code FROM MovementMapExcelImport ORDER BY Rev_2014 DESC LIMIT ' + offset + ', 1')
             lst = cursor.fetchone()
             place = getinfo(*lst)
-            url = place.website
-        except:
+            if place:
+                url = place.website
+        except Exception as e:
+            print(e)
             pass
+        
+        web_json  = {}
 
-        if url:
-            web_json = {} 
-            web_json['name'] = lst[0]
-            web_json['url'] = url
-            facebook_url = getfacebook(url)
-            twitter_url = gettwitter(url)
-            if facebook_url:
-               web_json['facebook'] = facebook_url
-            if twitter_url:
-               web_json['twitter'] = twitter_url
-            info_json.append(web_json)
-    return info_json
+        try:
+            if url:
+                web_json['Website'] = url
+                #web_json['location'] = place.formatted_address
+                
+                cursor.execute('SELECT OrganizationID FROM organization WHERE CRALegalName=\"{}\"'.format(lst[0]))
+                bn = int(cursor.fetchone()[0])
+
+                facebook_url = getfacebook(url)
+                twitter_url = gettwitter(url)
+                instagram_url = getinstagram(url)
+                if facebook_url:
+                   web_json['Facebook'] = facebook_url
+                if twitter_url:
+                   web_json['Twitter'] = twitter_url
+                if instagram_url:
+                   web_json['Instagram'] = instagram_url
+                info_json.append(web_json)
+            
+            for x in web_json:
+                cursor.execute('SELECT URLTypeId FROM url_type WHERE URLTypeName=\'{}\''.format(x))
+                name_id = cursor.fetchone()[0]
+                print(offset +' INSERT INTO organization_url (OrganizationID, URLTypeId, URL) VALUE ({}, {}, \'{}\')'.format(bn, name_id, web_json[x]))
+                cursor.execute('INSERT INTO organization_url (OrganizationID, URLTypeId, URL) VALUE ({}, {}, \'{}\')'.format(bn, name_id, web_json[x]))
+
+            if web_json:
+                db.commit()
+        except Exception as e:
+            print(e)
+            pass
+           
+            
+handler_event()
